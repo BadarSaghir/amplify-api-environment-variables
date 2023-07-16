e258a0f2baebbd7fe34a2add3e869376e15a466c
Badar <dataentrybadar@gmail.com>
2023-07-16 15:36:13 +0500

    added demo

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
-
+"use client"
 export default function Home() {
   return (
-    <main>
-      
-      
- <h1> {"CLIENT SIDE VARIABLE "+process.env.NEXT_PUBLIC_TEST}</h1> 
- <h1>  {"SERVER SIDE VARIABLE "+process.env.TEST}</h1> 
+    <div>
 
-     
-    </main>
+
+      <h1> {"CLIENT SIDE VARIABLE ========> " + process.env.NEXT_PUBLIC_TEST}</h1>
+      <h1>  {"SERVER SIDE VARIABLE ========> " + process.env?.TEST}</h1>
+
+
+    </div>
   )
 }
